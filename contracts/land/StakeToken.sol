e6ad0d99ce02935b881ab1a45755b20f681da24a
themastermindnktp <themastermindnktp@gmail.com>
2025-06-10 16:34:34 +0700

    fix bug

--- contracts/land/StakeToken.sol
+++ contracts/land/StakeToken.sol
@@ -200,7 +200,8 @@ ReentrancyGuardUpgradeable {
             revert NoSuccessor();
         }
 
-        if (IPrimaryToken(primaryToken).isStakeRewardingCompleted()) {
+        IPrimaryToken primaryTokenContract = IPrimaryToken(primaryToken);
+        if (primaryTokenContract.isStakeRewardingCompleted()) {
             revert InvalidPromoting();
         }
 
@@ -214,6 +215,7 @@ ReentrancyGuardUpgradeable {
         weights[msg.sender] = weights[msg.sender]
             .sub(_tokenToWeight(_value, interestAccumulation));
 
+        primaryTokenContract.safeApprove(address(successorContract), _value);
         successorContract.stake(msg.sender, _value);
 
         emit Promotion(msg.sender, _value);
@@ -231,12 +233,12 @@ ReentrancyGuardUpgradeable {
 
     function exclusiveDiscount() external view returns (Rate memory rate) {
         IPrimaryToken primaryTokenContract = IPrimaryToken(primaryToken);
-        uint256 totalStake = primaryTokenContract.totalStake();
+        uint256 globalStake = primaryTokenContract.totalStake();
 
         Rate memory primaryDiscount = primaryTokenContract.exclusiveDiscount();
         return Rate(
             primaryDiscount.value
-                .scale(totalStake - totalStake, totalStake << 1)
+                .scale(globalStake - totalStake, globalStake << 1)
                 .add(Constant.PRIMARY_TOKEN_BASE_DISCOUNT),
             primaryDiscount.decimals
         );
