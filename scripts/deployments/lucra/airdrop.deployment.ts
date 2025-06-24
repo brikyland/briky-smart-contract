3a1751b9295f472a8a7e901731451fca22ad3f3c
themastermindnktp <themastermindnktp@gmail.com>
2025-06-24 13:16:04 +0700

    add Airdrop

---
+++ scripts/deployments/lucra/airdrop.deployment.ts
@@ -0,0 +1,35 @@
+import { LedgerSigner } from '@anders-t/ethers-ledger';
+import { ethers, network, upgrades } from 'hardhat';
+import { deployAirdrop } from '../../../utils/deployments/lucra/airdrop';
+
+async function deployOrUpgradeAirdrop() {
+    const config = network.config as any;
+    const networkName = network.name.toUpperCase();
+    const signer = networkName == 'MAINNET'
+        ? new LedgerSigner(ethers.provider)
+        : (await ethers.getSigners())[0];
+    const Airdrop = await ethers.getContractFactory('Airdrop', signer);
+    const airdropAddress = config.airdropAddress ?
+        await (async () => {
+            await upgrades.upgradeProxy(
+                config.airdropAddress,
+                Airdrop,
+            );
+            console.log(`Contract Airdrop has been updated to address ${config.airdropAddress}`);
+            return config.airdropAddress;
+        })() :
+        await (async () => {
+            const airdrop = await deployAirdrop(signer);
+            console.log(`Contract Airdrop has been deployed to address ${airdrop.address}`);
+
+            return airdrop.address;
+        })();
+    console.log(`${networkName}_PASSPORT_TOKEN_ADDRESS=${airdropAddress}`);
+}
+
+deployOrUpgradeAirdrop()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
---
+++ utils/deployments/lucra/airdrop.ts
@@ -0,0 +1,8 @@
+import { ethers, upgrades } from "hardhat";
+
+export async function deployAirdrop(signer: any) {
+    const Airdrop = await ethers.getContractFactory('Airdrop', signer);
+    const airdrop = await upgrades.deployProxy(Airdrop);
+    await airdrop.deployed();
+    return airdrop;
+}
