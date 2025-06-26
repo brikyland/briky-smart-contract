b07cd636cb9d5e466404f69629d55fb6010cb2a2
themastermindnktp <themastermindnktp@gmail.com>
2025-06-26 16:32:15 +0700

    update initialization values

--- scripts/deployments/lucra/initialization.ts
+++ scripts/deployments/lucra/initialization.ts
@@ -5,13 +5,21 @@ export const Initialization = {
     PROMOTION_TOKEN_Name: 'Briky Lucra',
     PROMOTION_TOKEN_Symbol: 'LUCKY',
     PROMOTION_TOKEN_BaseURI: '',
-    PROMOTION_TOKEN_Fee: ethers.utils.parseEther(String(3e-10)),
+    // Mainnet
+    PROMOTION_TOKEN_Fee: ethers.utils.parseEther(String(0.0008)),
     PROMOTION_TOKEN_RoyaltyRate: ethers.utils.parseEther(String(0.06)),
+    // Testnet
+    // PROMOTION_TOKEN_Fee: ethers.utils.parseEther(String(3e-10)),
+    // PROMOTION_TOKEN_RoyaltyRate: ethers.utils.parseEther(String(0.06)),
 
     // Passport Token
     PASSPORT_TOKEN_Name: 'Briky Passport',
     PASSPORT_TOKEN_Symbol: 'PASSY',
     PASSPORT_TOKEN_BaseURI: '',
-    PASSPORT_TOKEN_Fee: ethers.utils.parseEther(String(150e-8)),
+    // Mainnet
+    PASSPORT_TOKEN_Fee: ethers.utils.parseEther(String(0.016)),
     PASSPORT_TOKEN_RoyaltyRate: ethers.utils.parseEther(String(0.06)),
+    // Testnet
+    // PASSPORT_TOKEN_Fee: ethers.utils.parseEther(String(150e-8)),
+    // PASSPORT_TOKEN_RoyaltyRate: ethers.utils.parseEther(String(0.06)),
 }
