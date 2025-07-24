e5afeb9a52b1cf918fcb189e66a855d00f3caf23
prometheus1129 <prometheus1129@proton.me>
2025-07-24 22:49:31 +0700

    Fix precision when parsing ether

--- utils/constant.ts
+++ utils/constant.ts
@@ -1,4 +1,5 @@
 import { parseEther } from "./blockchain";
+import { BigNumber } from "ethers";
 
 const MINUTE = 60;
 const HOUR = 60 * MINUTE;
@@ -7,7 +8,8 @@ const DAY = 24 * HOUR;
 export const Constant = {
     // Common
     COMMON_RATE_DECIMALS: 18,
-    COMMON_RATE_MAX_FRACTION: parseEther(1),
+    COMMON_RATE_MAX_FRACTION: parseEther("1"),
+    COMMON_INFINITE_TIMESTAMP: BigNumber.from(2).pow(40).sub(1),
 
     // Admin
     ADMIN_NUMBER: 5,
@@ -17,22 +19,26 @@ export const Constant = {
     ESTATE_TOKEN_CONFIRMATION_TIME_LIMIT: 30 * DAY,
     ESTATE_TOKEN_MAX_DECIMALS: 18,
 
+    // Estate Forger
+    ESTATE_FORGER_MINIMUM_SALE_DURATION: 30 * DAY,
+    ESTATE_FORGER_CONFIRMATION_TIME_LIMIT: 30 * DAY,
+
     // Primary Token
-    PRIMARY_TOKEN_BASE_DISCOUNT: parseEther(0.15),
+    PRIMARY_TOKEN_BASE_DISCOUNT: parseEther("0.15"),
 
-    PRIMARY_TOKEN_MAXIMUM_SUPPLY: parseEther(20_000_000_000),
-    PRIMARY_TOKEN_BACKER_ROUND: parseEther(100_000_000),
-    PRIMARY_TOKEN_CORE_TEAM: parseEther(1_000_000_000),
-    PRIMARY_TOKEN_EXTERNAL_TREASURY: parseEther(1_000_000_000),
-    PRIMARY_TOKEN_MARKET_MAKER: parseEther(2_270_000_000),
-    PRIMARY_TOKEN_PRIVATE_SALE_1: parseEther(30_000_000),
-    PRIMARY_TOKEN_PRIVATE_SALE_2: parseEther(50_000_000),
-    PRIMARY_TOKEN_PUBLIC_SALE: parseEther(500_000_000),
-    PRIMARY_TOKEN_SEED_ROUND: parseEther(50_000_000),
+    PRIMARY_TOKEN_MAXIMUM_SUPPLY: parseEther(String(20_000_000_000)),
+    PRIMARY_TOKEN_BACKER_ROUND: parseEther(String(100_000_000)),
+    PRIMARY_TOKEN_CORE_TEAM: parseEther(String(1_000_000_000)),
+    PRIMARY_TOKEN_EXTERNAL_TREASURY: parseEther(String(1_000_000_000)),
+    PRIMARY_TOKEN_MARKET_MAKER: parseEther(String(2_270_000_000)),
+    PRIMARY_TOKEN_PRIVATE_SALE_1: parseEther(String(30_000_000)),
+    PRIMARY_TOKEN_PRIVATE_SALE_2: parseEther(String(50_000_000)),
+    PRIMARY_TOKEN_PUBLIC_SALE: parseEther(String(500_000_000)),
+    PRIMARY_TOKEN_SEED_ROUND: parseEther(String(50_000_000)),
 
-    PRIMARY_TOKEN_STAKE_1_WAVE_REWARD: parseEther(2_000_000),
-    PRIMARY_TOKEN_STAKE_2_WAVE_REWARD: parseEther(3_000_000),
-    PRIMARY_TOKEN_STAKE_3_WAVE_REWARD: parseEther(4_000_000),
+    PRIMARY_TOKEN_STAKE_1_WAVE_REWARD: parseEther(String(2_000_000)),
+    PRIMARY_TOKEN_STAKE_2_WAVE_REWARD: parseEther(String(3_000_000)),
+    PRIMARY_TOKEN_STAKE_3_WAVE_REWARD: parseEther(String(4_000_000)),
 
     PRIMARY_TOKEN_STAKE_1_CULMINATING_WAVE: 750,
     PRIMARY_TOKEN_STAKE_2_CULMINATING_WAVE: 1500,
@@ -42,5 +48,5 @@ export const Constant = {
     STAKE_TOKEN_REWARD_FETCH_COOLDOWN: DAY - 5 * MINUTE,
 
     // Treasury
-    TREASURY_OPERATION_FUND_RATE: parseEther(0.2),
+    TREASURY_OPERATION_FUND_RATE: parseEther("0.2"),
 }
