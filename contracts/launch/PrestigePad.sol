2bd4afcf9d9d62b8ce01457b322a76763abc499f
prometheus1129 <prometheus1129@proton.me>
2025-08-04 20:44:38 +0700

    Minor fix

--- contracts/launch/PrestigePad.sol
+++ contracts/launch/PrestigePad.sol
@@ -470,6 +470,8 @@ ReentrancyGuardUpgradeable {
                     }
                 }
 
+                CurrencyHandler.receiveNative(totalNative);
+
                 if (fund.mainDenomination != 0) {
                     if (fund.mainCurrency == address(0)) {
                         totalNative += fund.mainDenomination * fund.totalQuantity;
@@ -477,12 +479,11 @@ ReentrancyGuardUpgradeable {
                         CurrencyHandler.allowERC20(fund.mainCurrency, reserveVaultAddress, fund.mainDenomination * fund.totalQuantity);
                     }
                 }
-
-                CurrencyHandler.receiveNative(totalNative);
             }
 
             IReserveVault(reserveVaultAddress).provideFund{value: totalNative}(cashbackFundId);
         }
+        
         CurrencyHandler.sendCurrency(
             currency,
             feeReceiver,
