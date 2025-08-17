3ee0233c0d757500cd6c63d448a28885f32551e2
prometheus1129 <prometheus1129@proton.me>
2025-08-17 11:14:34 +0700

    Add deploy script for new contracts. Use path config

--- scripts/deployments/land/initialization.ts
+++ scripts/deployments/land/initialization.ts
@@ -23,19 +23,4 @@ export const Initialization = {
     // Estate Token
     ESTATE_TOKEN_BaseURI: '',
     ESTATE_TOKEN_RoyaltyRate: parseEther("0.0003"),
-
-    // Primary Token
-    PRIMARY_TOKEN_Name: 'Briky Capital',
-    PRIMARY_TOKEN_Symbol: 'BRIK',
-    PRIMARY_TOKEN_LiquidationUnlockedAt: Date.parse('2026-01-01T00:00:00') / 1000,
-
-    // Stake Token
-    STAKE_TOKEN_Name_1: 'Briky Stake 2028',
-    STAKE_TOKEN_Symbol_1: 'sBRIK-28',
-
-    STAKE_TOKEN_Name_2: 'Briky Stake 2030',
-    STAKE_TOKEN_Symbol_2: 'sBRIK-30',
-
-    STAKE_TOKEN_Name_3: 'Briky Stake 2032',
-    STAKE_TOKEN_Symbol_3: 'sBRIK-32',
 }
---
+++ scripts/deployments/lux/projectMarketplace.deployment.ts
@@ -0,0 +1,51 @@
+import { LedgerSigner } from '@anders-t/ethers-ledger';
+import assert from 'assert';
+import { ethers, network, upgrades } from 'hardhat';
+import { deployProjectMarketplace } from '@utils/deployments/lux/projectMarketplace';
+
+async function deployOrUpgradeProjectMarketplace() {
+    const config = network.config as any;
+    const networkName = network.name.toUpperCase();
+    const signer = networkName == 'MAINNET'
+        ? new LedgerSigner(ethers.provider)
+        : (await ethers.getSigners())[0];
+    const ProjectMarketplace = await ethers.getContractFactory('ProjectMarketplace', signer);
+    const projectMarketplaceAddress = config.projectMarketplaceAddress ?
+        await (async () => {
+            await upgrades.upgradeProxy(
+                config.projectMarketplaceAddress,
+                ProjectMarketplace,
+            );
+            console.log(`Contract ProjectMarketplace has been updated to address ${config.projectMarketplaceAddress}`);
+            return config.projectMarketplaceAddress;
+        })() :
+        await (async () => {
+            const adminAddress = config.adminAddress;
+            assert.ok(
+                adminAddress,
+                `Missing ${networkName}_ADMIN_ADDRESS from environment variables!`
+            );
+            const projectTokenAddress = config.projectTokenAddress;
+            assert.ok(
+                projectTokenAddress,
+                `Missing ${networkName}_PROJECT_TOKEN_ADDRESS from environment variables!`
+            );
+
+            const projectMarketplace = await deployProjectMarketplace(
+                signer,
+                adminAddress,
+                projectTokenAddress,
+            );
+            console.log(`Contract ProjectMarketplace has been deployed to address ${projectMarketplace.address}`);
+
+            return projectMarketplace.address;
+        })();
+    console.log(`${networkName}_PROJECT_MARKETPLACE_ADDRESS=${projectMarketplaceAddress}`);
+}
+
+deployOrUpgradeProjectMarketplace()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
