86e6ec0b5102e5dd96813ae62ac0e8c4e6abda68
prometheus1129 <prometheus1129@proton.me>
2025-09-18 00:47:56 +0700

    Update deployment scripts

--- scripts/deployments/lux/projectMarketplace.deployment.ts
+++ scripts/deployments/lux/projectMarketplace.deployment.ts
@@ -1,18 +1,45 @@
 import { LedgerSigner } from '@anders-t/ethers-ledger';
-import { ethers, network } from 'hardhat';
-import { deployOrUpgradeAssetMarketplace } from './base/assetMarketplace.deployment';
+import { deployProjectMarketplace } from '@utils/deployments/lux/projectMarketplace';
+import assert from 'assert';
+import { ethers, network, upgrades } from 'hardhat';
 
 async function deployOrUpgradeProjectMarketplace() {
+    const config = network.config as any;
     const networkName = network.name == 'localhost' ? 'LOCAL' : network.name.toUpperCase();
     const signer = networkName == 'MAINNET'
         ? new LedgerSigner(ethers.provider)
         : (await ethers.getSigners())[0];
-    const projectMarketplaceAddress = await deployOrUpgradeAssetMarketplace(
-        signer,
-        'projectToken',
-        'PROJECT_TOKEN',
-        'Project',
-    );
+    const ProjectMarketplace = await ethers.getContractFactory('ProjectMarketplace', signer);
+    const projectMarketplaceAddress = config.projectMarketplaceAddress ?
+        await (async () => {
+            await upgrades.upgradeProxy(
+                config.projectMarketplaceAddress,
+                ProjectMarketplace,
+            );
+            console.log(`Contract ProjectMarketplace has been updated to address ${config.projectMarketplaceAddress}`);
+            return config.projectMarketplaceAddress;
+        })() :
+        await (async () => {
+            const adminAddress = config.adminAddress;
+            assert.ok(
+                adminAddress,
+                `Missing ${networkName}_ADMIN_ADDRESS from environment variables!`
+            );
+            const projectTokenAddress = config[`projectTokenAddress`];
+            assert.ok(
+                projectTokenAddress,
+                `Missing ${networkName}_PROJECT_TOKEN_ADDRESS from environment variables!`
+            );
+
+            const projectMarketplace = await deployProjectMarketplace(
+                signer,
+                adminAddress,
+                projectTokenAddress,
+            );
+            console.log(`Contract ProjectMarketplace has been deployed to address ${projectMarketplace.address}`);
+
+            return projectMarketplace.address;
+        })();
     console.log(`${networkName}_PROJECT_MARKETPLACE_ADDRESS=${projectMarketplaceAddress}`);
 }
 
