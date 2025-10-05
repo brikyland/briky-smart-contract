505a20396e9a6c897178783289f5fbbf0f8796a3
prometheus1129 <prometheus1129@proton.me>
2025-10-05 18:37:48 +0700

    Update test

--- utils/transaction/liquidity/auction.ts
+++ utils/transaction/liquidity/auction.ts
@@ -1,6 +1,8 @@
-import {Auction} from "@typechain-types";
-import {StartAuctionParams, UpdateStakeTokensParams} from "@utils/models/liquidity/auction";
+import {Admin, Auction} from "@typechain-types";
+import {DepositParams, StartAuctionParams, StartAuctionParamsInput, UpdateStakeTokensParams, UpdateStakeTokensParamsInput} from "@utils/models/liquidity/auction";
 import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
+import { getStartAuctionSignatures, getUpdateStakeTokensSignatures } from "@utils/signatures/liquidity/auction";
+
 
 // updateStakeTokens
 export async function getUpdateStakeTokensTx(
@@ -18,14 +20,72 @@ export async function getUpdateStakeTokensTx(
     );
 }
 
+export async function getUpdateStakeTokensTxByInput(
+    auction: Auction,
+    deployer: SignerWithAddress,
+    paramsInput: UpdateStakeTokensParamsInput,
+    admin: Admin,
+    admins: any[],
+    txConfig = {}
+) {
+    const params: UpdateStakeTokensParams = {
+        ...paramsInput,
+        signatures: await getUpdateStakeTokensSignatures(auction, paramsInput, admin, admins)
+    };
+    return getUpdateStakeTokensTx(auction, deployer, params, txConfig);
+}
+
+
+// startAuction
 export async function getStartAuctionTx(
     auction: Auction,
     deployer: SignerWithAddress,
-    params: StartAuctionParams
+    params: StartAuctionParams,
+    txConfig = {}
 ) {
     return auction.connect(deployer).startAuction(
         params.endAt,
         params.vestingDuration,
         params.signatures,
+        txConfig
     );
+}
+
+export async function getStartAuctionTxByInput(
+    auction: Auction,
+    deployer: SignerWithAddress,
+    paramsInput: StartAuctionParamsInput,
+    admin: Admin,
+    admins: any[],
+    txConfig = {}
+) {
+    const params: StartAuctionParams = {
+        ...paramsInput,
+        signatures: await getStartAuctionSignatures(auction, paramsInput, admin, admins)
+    };
+    return getStartAuctionTx(auction, deployer, params, txConfig);
+}
+
+
+// deposit
+export async function getDepositTx(
+    auction: Auction,
+    deployer: SignerWithAddress,
+    params: DepositParams,
+    txConfig = {}
+) {
+    return auction.connect(deployer).deposit(
+        params.value,
+        txConfig
+    );
+}
+
+
+// withdraw
+export async function getWithdrawTx(
+    auction: Auction,
+    deployer: SignerWithAddress,
+    txConfig = {}
+) {
+    return auction.connect(deployer).withdraw(txConfig);
 }
